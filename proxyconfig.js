bb6d8b59e54ad2bff6969fc1af69eaf2ab0cf18c
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-04-06 01:04:46 +0800

    updated proxy config to support local dev

--- proxyconfig.js
+++ proxyconfig.js
@@ -1,17 +1,21 @@
 module.exports = {
   "/": {
     target: "https://theatreapi.saileshkumar.com",
+    // target: "http://localhost:3000",
     changeOrigin: true,
     secure: false,
     cookieDomainRewrite: "localhost",
     debug: true,
-    onProxyRes: proxyResponse => {
+    onProxyRes: (proxyResponse) => {
       if (proxyResponse.headers["set-cookie"]) {
-        const cookies = proxyResponse.headers["set-cookie"].map(cookie => {
-          return cookie.replace(/; secure; SameSite=None/gi, "");
+        const cookies = proxyResponse.headers["set-cookie"].map((cookie) => {
+          let newCookie;
+          newCookie = cookie.replace(/; secure/gi, "");
+          newCookie = cookie.replace(/; SameSite=None/gi, "");
+          return newCookie;
         });
         proxyResponse.headers["set-cookie"] = cookies;
       }
-    }
-  }
+    },
+  },
 };
