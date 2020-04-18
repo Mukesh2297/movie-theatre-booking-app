4faeb6c574b6ce3576e92aef9fc7cd1c9eeddf7f
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-04-19 03:32:26 +0800

    updated proxyconfig

--- proxyconfig.js
+++ proxyconfig.js
@@ -1,18 +1,21 @@
 module.exports = {
-  "/": {
+  "/api": {
     target: "https://theatreapi.saileshkumar.com",
     // target: "http://localhost:3000",
     changeOrigin: true,
     secure: false,
     cookieDomainRewrite: "localhost",
     debug: true,
-    onProxyRes: proxyResponse => {
+    pathRewrite: function (path, req) {
+      return path.replace("/api", "");
+    },
+    onProxyRes: (proxyResponse) => {
       if (proxyResponse.headers["set-cookie"]) {
-        const cookies = proxyResponse.headers["set-cookie"].map(cookie => {
+        const cookies = proxyResponse.headers["set-cookie"].map((cookie) => {
           return cookie.replace(/; secure; SameSite=None/gi, "");
         });
         proxyResponse.headers["set-cookie"] = cookies;
       }
-    }
-  }
+    },
+  },
 };
