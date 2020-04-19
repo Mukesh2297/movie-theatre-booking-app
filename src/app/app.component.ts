60fb7a0657f903f2dff92a6473433f140d59333f
Mukesh Raj <58542409+Mukesh2297@users.noreply.github.com>
2020-04-19 21:35:01 +0530

    reactiveforms handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title = 'seats-booking';
 
   constructor(private router:Router)
