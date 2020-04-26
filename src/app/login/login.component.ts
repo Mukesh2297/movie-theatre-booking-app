53c64fce90c36d7aed4f0f34e3a70d5e24b3d126
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-04-26 22:39:31 +0800

    captcha fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -103,9 +103,7 @@ export class LoginComponent implements OnInit {
         });
         this.signupform = false;
         this.loginform = true;
-      } else if (response.status === 'Server error') {
-        this.apiRegisterResponse = 'Something went wrong. Please try again';
-      } else if (response.status === 'Unauthorized' && response.captcha) {
+      } else {
         this.captchaSvg = this.sanitized.bypassSecurityTrustHtml(
           response.captcha
         );
